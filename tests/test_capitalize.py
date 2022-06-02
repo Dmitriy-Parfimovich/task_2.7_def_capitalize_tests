04708abf4d4f1fefadc412626e5cfcad7a9e73c3
Dmitriy Parfimovich <dparf@mail.ru>
2022-06-02 21:34:23 +0300

    task 2.8 add assert

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,9 +1,12 @@
 from capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
 
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
+assert capitalize("hello") == "Hello"
+#if capitalize('hello') != 'Hello':
+    #raise Exception('Функция работает неверно!')
+
+assert capitalize("") == ""
+#if capitalize('') != '':
+    #raise Exception('Функция работает неверно!')
 
 print('Все тесты пройдены!')
